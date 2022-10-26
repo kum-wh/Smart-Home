19a6f4570a945c018a4ed88806bbdb52aa4a26ee
Zhi Xiang <pzhixiang.99@gmail.com>
2022-10-26 14:07:12 +0800

    Squashed commit of the following:
    commit b1a165fa8b396ff47191636adf424340220b52ad
    Author: Zhi Xiang <pzhixiang.99@gmail.com>
    Date:   Wed Oct 26 13:40:37 2022 +0800
    
        Modify .gitignore
    
    commit c75d6ce63df05f50e7dc63745c1f9131e667fafd
    Author: Zhi Xiang <pzhixiang.99@gmail.com>
    Date:   Wed Oct 26 13:36:52 2022 +0800
    
        Add emotion recognition model to .gitignore
    
    commit 982418911d24f1bb8267b77d3082f14c63fa3c63
    Author: Zhi Xiang <pzhixiang.99@gmail.com>
    Date:   Wed Oct 26 02:26:32 2022 +0800
    
        Integrate Emotion Recognition Model

--- smart_home/backend/views.py
+++ smart_home/backend/views.py
@@ -1,11 +1,13 @@
 import urllib
-import cv2
-# from keras.models import load_model
+# import cv2
+from keras.models import load_model
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
+import numpy as np
+from PIL import Image
 
-# emotion_model = load_model()
+emotion_model = load_model("model/emotions_v2.hd5")
 # security_model = load_model()
 
 # Create your views here.
@@ -26,18 +28,35 @@ def emotion(request):
         output = open("emotion.jpg","wb")
         output.write(data)
         output.close()
-        image =  cv2.imread("emotion.jpg")
+
+        emotion_dict = {0: 'angry', 1: 'happy', 2: 'neutral', 3: 'sad'}
+        image = Image.open('emotion.jpg')
+        image = image.convert('L')
+        image = image.resize((48, 48), resample=Image.LANCZOS)
+        img_array = np.array(image) / 255.0
+        final_array = np.expand_dims(img_array, axis = 0)
+        image = image.save('emotion.jpg', quality=95)
+
+        # image =  cv2.imread("emotion.jpg")
+        
+        result = emotion_model.predict(final_array)
+        max_class = np.argmax(result)
+        print("Detected %s with confidence %3.2f" % (emotion_dict[max_class], result[0][max_class]))
+
         '''
-        result = model.predict(image)
-        if result == "anger":
+        if max_class == "angry":
             colour = 1"purple"
-        elif result == "sad":
+        elif max_class == "happy":
             colour = 2 "blue"
+        elif max_class == "neutral":
+            colour = 
         else:
             colour == 0 "white"
-        state = State.objects.get(id=0)
-        state.emotion = colour
         '''
+
+        # state = State.objects.get(id=0)
+        # state.emotion = emotion_dict[themax]
+        
     return render(request, "emotion.html")
 
 
@@ -49,7 +68,7 @@ def security(request):
         output = open("security.jpg","wb")
         output.write(data)
         output.close()
-        image =  cv2.imread("security.jpg")
+        # image =  cv2.imread("security.jpg")
         '''
         number = model.predict(image)
         state = State.objects.get(id=0)
