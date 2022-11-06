097cefd38158e028b840dd0c5ee95802b16d1116
Zhi Xiang <pzhixiang.99@gmail.com>
2022-11-07 02:18:15 +0800

    Change email settings in settings.py

--- smart_home/smart_home/settings.py
+++ smart_home/smart_home/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django_ses.SESBackend'
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'email-smtp.ap-southeast-1.amazonaws.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('AWS_ACCESS_KEY_ID')
+EMAIL_HOST_PASSWORD = env('AWS_SECRET_ACCESS_KEY')
