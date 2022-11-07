44438def6536e4af1a26fdee3c4ca503d1282a1f
kum-wh <wingho32@gmail.com>
2022-11-07 17:16:38 +0800

    Merge Login into main

--- smart_home/backend/views.py
+++ smart_home/backend/views.py
@@ -4,6 +4,11 @@ import cv2
 from keras.models import load_model
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.core.mail import send_mail
 from .models import *
 import numpy as np
@@ -18,16 +23,21 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 # Create your views here.
-
 def index(request):
+    return render(request, "index.html")
+
+
+@login_required(login_url="/login")
+def home(request):
     if request.method == "POST":
         number = request.POST.get('rnumber')
         state = State.objects.get(id=1)
         state.number = number
         state.save()
-    return render(request, "index.html")
+    return render(request, "home.html")
 
 
+@login_required(login_url="/login")
 def emotion(request):
     if request.method == "POST":
         link = request.POST.get('image')
@@ -67,6 +77,7 @@ def emotion(request):
     return render(request, "emotion.html")
 
 
+@login_required(login_url="/login")
 def security(request):
     if request.method == "POST":
 
@@ -150,9 +161,9 @@ def getemo(request):
     
     state = State.objects.get(id=1)
     value = state.emotion
-    
-    # value = 0
+
     return HttpResponse(f'{value}')
+
 
 @csrf_exempt
 def slave(request):
@@ -166,3 +177,54 @@ def slave(request):
 
     return HttpResponse(f'{slave}')
 
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return render(request, "register.html")
